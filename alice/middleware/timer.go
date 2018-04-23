28afa3f505882faca5c705b6bdc82382362363cf
Romesh McCullough <romesh.mccullough@gmail.com>
2018-04-23 09:52:44 -0400

    add null time middleware

--- alice/middleware/timer.go
+++ alice/middleware/timer.go
@@ -1,10 +1,26 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/justinas/alice"
 )
 
 // Timer can time a handler and log it
 type Timer interface {
 	Time(name string) alice.Constructor
+}
+
+type nullTimer struct{}
+
+func (n *nullTimer) Time(name string) alice.Constructor {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			h.ServeHTTP(w, r)
+		})
+	}
+}
+
+func NewNullTimer() Timer {
+	return &nullTimer{}
 }

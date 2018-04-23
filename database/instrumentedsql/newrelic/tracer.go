587d31d75ea4f3691ec38c5676e003fae9108f44
Romesh McCullough <romi@MacBook-Pro-2.lan>
2018-04-23 09:19:25 -0400

    add instrumentedsql newrelic tracer

---
+++ database/instrumentedsql/newrelic/tracer.go
@@ -0,0 +1,56 @@
+package newrelic
+
+import (
+	"context"
+
+	"github.com/ExpansiveWorlds/instrumentedsql"
+	"github.com/healthimation/go-service/internal"
+	newrelic "github.com/newrelic/go-agent"
+)
+
+type contextKey string
+
+type tracer struct {
+}
+
+type span struct {
+	txn     newrelic.Transaction
+	segment newrelic.DatastoreSegment
+}
+
+// NewTracer returns a tracer that will fetch spans using opentracing's SpanFromContext function
+func NewTracer() instrumentedsql.Tracer {
+	return tracer{}
+}
+
+// GetSpan will always return a span with an EXISTING transaction.  TODO: start transaction
+func (tracer) GetSpan(ctx context.Context) instrumentedsql.Span {
+	transaction := ctx.Value(internal.ContextKeyNewrelicTransaction)
+	if t, ok := transaction.(newrelic.Transaction); ok {
+		return span{txn: t}
+	}
+	return span{}
+}
+
+func (s span) NewChild(name string) instrumentedsql.Span {
+	if s.txn != nil {
+		s.segment = newrelic.DatastoreSegment{
+			StartTime: newrelic.StartSegmentNow(s.txn),
+			Product:   newrelic.DatastorePostgres,
+			Operation: name,
+		}
+	}
+
+	return s
+}
+
+func (s span) SetLabel(k, v string) {
+	if k == "query" {
+		s.segment.ParameterizedQuery = v
+	}
+	return
+}
+
+func (s span) Finish() {
+	s.segment.End()
+}

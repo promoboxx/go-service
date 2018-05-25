e910fa93619c5cbebe3a62cd37cf6c97dbfac800
Kyle <kyle.boorky@gmail.com>
2018-05-25 11:52:33 -0400

    adding middleware to add no cache headers

---
+++ alice/middleware/no-cache.go
@@ -0,0 +1,16 @@
+package middleware
+
+import "net/http"
+
+// NoCache will add the following headers to the response:
+// 	Cache-Control: no-cache, no-store, must-revalidate
+// 	Pragma: no-cache
+// 	Expires: 0
+func NoCache(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate") // HTTP 1.1
+		w.Header().Set("Pragma", "no-cache")                                   // HTTP 1.0
+		w.Header().Set("Expires", "0")                                         // Proxies
+		h.ServeHTTP(w, r)
+	})
+}

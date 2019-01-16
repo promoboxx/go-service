658b8d5466eed88f4bed02d1777ae1043b0e556f
Thomas Sharkey <tomsharkey@Thomass-MacBook-Pro.local>
2019-01-16 14:55:47 -0700

    reworking how the ctx is set and and the fields for the log message

--- alice/middleware/logger.go
+++ alice/middleware/logger.go
@@ -35,6 +35,13 @@ func NewLogrusLogger(baseEntry *logrus.Entry, logRequests bool) Logger {
 func (l *logger) Log(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		loggingResponseWriter := &lrw.LoggingResponseWriter{w, http.StatusOK, nil}
+
+		// add logger to the context
+		ctx := context.WithValue(r.Context(), contextKeyLogger, entry)
+		r = r.WithContext(ctx)
+
+		h.ServeHTTP(loggingResponseWriter, r)
+
 		entry := l.entry.WithFields(logrus.Fields{
 			logFieldRequestID:  getRequestIDFromContext(r.Context()),
 			logFieldUserID:     getInsecureUserIDFromContext(r.Context()),
@@ -43,12 +50,6 @@ func (l *logger) Log(h http.Handler) http.Handler {
 			logFieldPath:       r.URL.String(),
 			logFieldError:      loggingResponseWriter.InnerError,
 		})
-
-		// add logger to the context
-		ctx := context.WithValue(r.Context(), contextKeyLogger, entry)
-		r = r.WithContext(ctx)
-
-		h.ServeHTTP(loggingResponseWriter, r)
 
 		if l.logRequests {
 			entry.Printf("Finished request")

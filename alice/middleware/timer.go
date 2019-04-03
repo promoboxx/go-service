facc78bca481fa26c0ad85b4ce2c04abeba27063
Thomas Sharkey <tomsharkey@Thomass-MacBook-Pro.local>
2019-04-03 15:52:10 -0600

    adding a span timer

--- alice/middleware/timer.go
+++ alice/middleware/timer.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/justinas/alice"
+	"github.com/opentracing/opentracing-go"
 )
 
 // Timer can time a handler and log it
@@ -23,4 +24,21 @@ func (n *nullTimer) Time(name string) alice.Constructor {
 
 func NewNullTimer() Timer {
 	return &nullTimer{}
+}
+
+type openTracingTimer struct {
+}
+
+func NewOpenTracingTimer() Timer {
+	return &openTracingTimer{}
+}
+
+func (ott *openTracingTimer) Time(name string) alice.Constructor {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			span, ctx := opentracing.StartSpanFromContext(ctx, name)
+			defer span.Finish()
+		})
+	}
 }

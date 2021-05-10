7010d53e56f9603c5ebc3ac79d44068b6e8e967c
Charles Gude <gudetimes1234@gmail.com>
2021-05-10 13:54:27 -0700

    use googleuuid.Parse instead of regex for parsing uuids

--- uuid/validate.go
+++ uuid/validate.go
@@ -1,10 +1,14 @@
 package uuid
 
-import "regexp"
+import googleuuid "github.com/google/uuid"
 
 // IsValid returns true if the uuid string provided is in a valid format
 // Note that this function was copied from https://stackoverflow.com/questions/25051675/how-to-validate-uuid-v4-in-go
 func IsValid(uuid string) bool {
-	r := regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
-	return r.MatchString(uuid)
+	var isValid bool
+	_, err := googleuuid.Parse(uuid)
+	if err == nil {
+		isValid = true
+	}
+	return isValid
 }

f106a364e29b32091bdfb41e31ed922c95ec5eaa
Charles Gude <gudetimes1234@gmail.com>
2021-05-12 09:07:19 -0700

    updates for CR

--- uuid/validate.go
+++ uuid/validate.go
@@ -5,10 +5,7 @@ import googleuuid "github.com/google/uuid"
 // IsValid returns true if the uuid string provided is in a valid format
 // Note that this function was copied from https://stackoverflow.com/questions/25051675/how-to-validate-uuid-v4-in-go
 func IsValid(uuid string) bool {
-	var isValid bool
 	_, err := googleuuid.Parse(uuid)
-	if err == nil {
-		isValid = true
-	}
-	return isValid
+
+	return err == nil
 }

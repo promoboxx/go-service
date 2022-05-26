90c16a113dc187704bdae06d67cd41754df2b708
Tom Sharkey <tom@promoboxx.com>
2022-05-26 12:15:11 -0400

    add return for auth middleware error

--- alice/middleware/authentication.go
+++ alice/middleware/authentication.go
@@ -9,18 +9,18 @@ import (
 	"github.com/promoboxx/go-service/service"
 )
 
-func Auth(token auth.Token)	func(http.Handler) http.Handler {
+func Auth(token auth.Token) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			claims, err := token.ValidateJWT(r.Header.Get("Authorization"))
 			if err != nil {
-				service.WriteProblem(w, "Could not validate JWT", "NOT_AUTHORIZED" , http.StatusUnauthorized, err)
-			}   
-			
+				service.WriteProblem(w, "Could not validate JWT", "NOT_AUTHORIZED", http.StatusUnauthorized, err)
+				return
+			}
+
 			ctx := context.WithValue(r.Context(), contextkey.ContextKeyClaims, claims)
-			
+
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
 }
-	

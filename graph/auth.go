9c0052a3bec4f25d28b417ade6bc677d2a113d79
Nick Salesky <nicksalesky@gmail.com>
2022-08-02 10:06:56 -0700

    Added an error to auth

--- graph/auth.go
+++ graph/auth.go
@@ -2,6 +2,7 @@ package graph
 
 import (
 	"context"
+	"errors"
 
 	"github.com/promoboxx/go-auth/src/auth"
 	"github.com/promoboxx/go-service/alice/middleware"
@@ -11,13 +12,12 @@ import (
 // to gracefully handle situations where no auth claims were injected by any middleware. This change is necessary
 // because the GraphQL middleware does not prevent requests from reaching the resolvers if no token is present,
 // so they could panic with an unhelpful error message if they tried to call the standard function
-func EnsureAuthenticated(ctx context.Context) (c *auth.Claim) {
+func EnsureAuthenticated(ctx context.Context) (c auth.Claim, e error) {
 	defer func() {
 		if err := recover(); err != nil {
-			c = nil // the return value is named so it can be modified here, pretty cool
+			e = errors.New("unauthorized access")
 		}
 	}()
 
-	claim := middleware.GetClaimsFromContext(ctx)
-	return &claim
+	return middleware.GetClaimsFromContext(ctx), nil
 }

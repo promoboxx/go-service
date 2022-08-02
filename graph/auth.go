b5da9a12ab9023cceeae0524aa6e394e664dc622
Nick Salesky <nicksalesky@gmail.com>
2022-08-02 10:09:05 -0700

    Updated comment

--- graph/auth.go
+++ graph/auth.go
@@ -8,7 +8,7 @@ import (
 	"github.com/promoboxx/go-service/alice/middleware"
 )
 
-// This is a bit of a hack workaround to make the `middleware.GetClaimsFromContext` function return a pointer,
+// This is a bit of a hack workaround to make the `middleware.GetClaimsFromContext` function return an error,
 // to gracefully handle situations where no auth claims were injected by any middleware. This change is necessary
 // because the GraphQL middleware does not prevent requests from reaching the resolvers if no token is present,
 // so they could panic with an unhelpful error message if they tried to call the standard function

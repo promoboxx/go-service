8dd6adccb213d79e022cb39271a00fda71194ed9
Tom Sharkey <sharkey.thomasm@gmail.com>
2024-05-24 07:53:02 -0400

    remove vendor folder

--- vendor/github.com/promoboxx/go-cache/src/cache/storage.go
+++
@@ -1,9 +0,0 @@
-package cache
-
-import "time"
-
-// Storage provides Get and Set to a cache backend
-type Storage interface {
-	Get(key string, value interface{}) error
-	Set(key string, value interface{}, expiration time.Duration) error
-}
